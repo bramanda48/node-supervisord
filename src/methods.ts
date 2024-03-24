62040ce5f02d79e735ebe9bc9d8008e5ea2a8987
Bramanda Suwandi <bramanda48@gmail.com>
2024-03-25 01:16:19 +0700

    feat: :pencil2: change Property Declaration to Method Declaration

--- src/methods.ts
+++ src/methods.ts
@@ -53,7 +53,7 @@ export interface SupervisordClientMethod {
    *
    * @return {string}
    */
-  getAPIVersion: () => Promise<string>;
+  getAPIVersion(): Promise<string>;
 
   /**
    * @deprecated
@@ -61,35 +61,35 @@ export interface SupervisordClientMethod {
    *
    * @return {string}
    */
-  getVersion: () => Promise<string>;
+  getVersion(): Promise<string>;
 
   /**
    * Return the version of the supervisor package in use by supervisord
    *
    * @return {string} version id
    */
-  getSupervisorVersion: () => Promise<string>;
+  getSupervisorVersion(): Promise<string>;
 
   /**
    * Return identifying string of supervisord
    *
    * @return {string} identifying string
    */
-  getIdentification: () => Promise<string>;
+  getIdentification(): Promise<string>;
 
   /**
    * Return current state of supervisord as a struct
    *
    * @return {State} A struct with keys int statecode, string statename
    */
-  getState: () => Promise<State>;
+  getState(): Promise<State>;
 
   /**
    * Return the PID of supervisord
    *
    * @return {number} PID
    */
-  getPID: () => Promise<number>;
+  getPID(): Promise<number>;
 
   /**
    * Read length bytes from the main log starting at offset
@@ -98,28 +98,28 @@ export interface SupervisordClientMethod {
    * @param {number} length number of bytes to read from the log
    * @return {string} Bytes of log
    */
-  readLog: (offset: number, length: number) => Promise<string>;
+  readLog(offset: number, length: number): Promise<string>;
 
   /**
    * Clear the main log
    *
    * @return {boolean} always returns True unless error
    */
-  clearLog: () => Promise<boolean>;
+  clearLog(): Promise<boolean>;
 
   /**
    * Shut down the supervisor process
    *
    * @return {boolean} always returns True unless error
    */
-  shutdown: () => Promise<boolean>;
+  shutdown(): Promise<boolean>;
 
   /**
    * Restart the supervisor process
    *
    * @return {boolean} always returns True unless error
    */
-  restart: () => Promise<boolean>;
+  restart(): Promise<boolean>;
 
   /**
    * Get info about a process named name
@@ -127,21 +127,21 @@ export interface SupervisordClientMethod {
    * @param {string} name The name of the process (or `group:name`)
    * @return {ProcessInfo}  A structure containing data about the process
    */
-  getProcessInfo: (name: string) => Promise<ProcessInfo>;
+  getProcessInfo(name: string): Promise<ProcessInfo>;
 
   /**
    * Get info about all processes
    *
    * @return {ProcessInfo[]} An array of process status results
    */
-  getAllProcessInfo: () => Promise<ProcessInfo[]>;
+  getAllProcessInfo(): Promise<ProcessInfo[]>;
 
   /**
    * Get info about all available process configurations. Each struct represents a single process (i.e. groups get flattened)
    *
    * @return {ConfigInfo[]} An array of process config info structs
    */
-  getAllConfigInfo: () => Promise<ConfigInfo[]>;
+  getAllConfigInfo(): Promise<ConfigInfo[]>;
 
   /**
    * Start a process
@@ -150,7 +150,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully started `(optional)` `(default: true)`
    * @return {boolean} always returns True unless error
    */
-  startProcess: (name: string, wait?: boolean) => Promise<boolean>;
+  startProcess(name: string, wait?: boolean): Promise<boolean>;
 
   /**
    * Start all processes listed in the configuration file
@@ -158,7 +158,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully started `(optional)` `(default: true)`
    * @return {ProcessStatusInfo[]} An array of process status info structs
    */
-  startAllProcesses: (wait?: boolean) => Promise<ProcessStatusInfo[]>;
+  startAllProcesses(wait?: boolean): Promise<ProcessStatusInfo[]>;
 
   /**
    * Start all processes in the group named `name`
@@ -167,7 +167,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully started `(optional)` `(default: true)`
    * @return {ProcessStatusInfo} An array of process status info structs
    */
-  startProcessGroup: (name: string, wait?: boolean) => Promise<ProcessStatusInfo[]>;
+  startProcessGroup(name: string, wait?: boolean): Promise<ProcessStatusInfo[]>;
 
   /**
    * Stop a process named by name
@@ -176,7 +176,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully stopped `(optional)` `(default: true)`
    * @return {boolean} always returns True unless error
    */
-  stopProcess: (name: string, wait?: boolean) => Promise<boolean>;
+  stopProcess(name: string, wait?: boolean): Promise<boolean>;
 
   /**
    * Stop all processes in the process group named `name`
@@ -185,7 +185,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully stopped `(optional)` `(default: true)`
    * @return {ProcessStatusInfo[]} always returns True unless error
    */
-  stopProcessGroup: (name: string, wait?: boolean) => Promise<ProcessStatusInfo[]>;
+  stopProcessGroup(name: string, wait?: boolean): Promise<ProcessStatusInfo[]>;
 
   /**
    * Stop all processes in the process list
@@ -193,7 +193,7 @@ export interface SupervisordClientMethod {
    * @param {boolean} wait Wait for process to be fully started `(optional)` `(default: true)`
    * @return {ProcessStatusInfo[]} An array of process status info structs
    */
-  stopAllProcesses: (wait?: boolean) => Promise<ProcessStatusInfo[]>;
+  stopAllProcesses(wait?: boolean): Promise<ProcessStatusInfo[]>;
 
   /**
    * Send an arbitrary UNIX signal to the process named by name
@@ -202,7 +202,7 @@ export interface SupervisordClientMethod {
    * @param {string | number} signal Signal to send, as name (`HUP`) or number (`1`)
    * @return {boolean} true on success
    */
-  signalProcess: (name: string, signal: string | number) => Promise<boolean>;
+  signalProcess(name: string, signal: string | number): Promise<boolean>;
 
   /**
    * Send a signal to all processes in the group named `name`
@@ -211,7 +211,7 @@ export interface SupervisordClientMethod {
    * @param {string | number} signal Signal to send, as name (`HUP`) or number (`1`)
    * @return {ProcessStatusInfo[]} An array of process status info structs
    */
-  signalProcessGroup: (name: string, signal: string | number) => Promise<ProcessStatusInfo[]>;
+  signalProcessGroup(name: string, signal: string | number): Promise<ProcessStatusInfo[]>;
 
   /**
    * Send a string of chars to the stdin of the process name
@@ -224,7 +224,7 @@ export interface SupervisordClientMethod {
    * @param {string} chars The character data to send to the process
    * @return {boolean} always returns True unless error
    */
-  sendProcessStdin: (name: string, chars: string) => Promise<boolean>;
+  sendProcessStdin(name: string, chars: string): Promise<boolean>;
 
   /**
    * Send an event that will be received by event listener subprocesses subscribing to the RemoteCommunicationEvent
@@ -233,7 +233,7 @@ export interface SupervisordClientMethod {
    * @param {string} data Data for the event body
    * @return {boolean} always returns True unless error
    */
-  sendRemoteCommEvent: (type: string, data: string) => Promise<boolean>;
+  sendRemoteCommEvent(type: string, data: string): Promise<boolean>;
 
   /**
    * Reload the configuration. The result contains three arrays containing names of process groups:
@@ -243,7 +243,7 @@ export interface SupervisordClientMethod {
    *
    * @return {any} `[[added, changed, removed]]`
    */
-  reloadConfig: () => Promise<any>;
+  reloadConfig(): Promise<any>;
 
   /**
    * Update the config for a running process from config file
@@ -251,7 +251,7 @@ export interface SupervisordClientMethod {
    * @param {string} name Name of process group to add
    * @return {boolean} true on success
    */
-  addProcessGroup: (name: string) => Promise<boolean>;
+  addProcessGroup(name: string): Promise<boolean>;
 
   /**
    * Remove a stopped process from the active configuration
@@ -259,7 +259,7 @@ export interface SupervisordClientMethod {
    * @param {string} name Name of process group to remove
    * @return {boolean} Indicates whether the removal was successful
    */
-  removeProcessGroup: (name: string) => Promise<boolean>;
+  removeProcessGroup(name: string): Promise<boolean>;
 
   /**
    * Read length bytes from name’s stdout log starting at offset
@@ -269,7 +269,7 @@ export interface SupervisordClientMethod {
    * @param {number} length Number of bytes to read from the log
    * @return {string} Bytes of log
    */
-  readProcessStdoutLog: (name: string, offset: number, length: number) => Promise<string>;
+  readProcessStdoutLog(name: string, offset: number, length: number): Promise<string>;
 
   /**
    * Read length bytes from name’s stderr log starting at offset
@@ -279,7 +279,7 @@ export interface SupervisordClientMethod {
    * @param {number} length Number of bytes to read from the log
    * @return {string} Bytes of log
    */
-  readProcessStderrLog: (name: string, offset: number, length: number) => Promise<string>;
+  readProcessStderrLog(name: string, offset: number, length: number): Promise<string>;
 
   /**
    * Provides a more efficient way to tail the (stdout) log than readProcessStdoutLog().
@@ -296,7 +296,7 @@ export interface SupervisordClientMethod {
    * @param {number} length Maximum number of bytes to return
    * @return {any} `[string bytes, int offset, bool overflow]`
    */
-  tailProcessStdoutLog: (name: string, offset: number, length: number) => Promise<any>;
+  tailProcessStdoutLog(name: string, offset: number, length: number): Promise<any>;
 
   /**
    * Provides a more efficient way to tail the (stderr) log than readProcessStderrLog().
@@ -313,7 +313,7 @@ export interface SupervisordClientMethod {
    * @param {number} length Maximum number of bytes to return
    * @return {any} `[string bytes, int offset, bool overflow]`
    */
-  tailProcessStderrLog: (name: string, offset: number, length: number) => Promise<any>;
+  tailProcessStderrLog(name: string, offset: number, length: number): Promise<any>;
 
   /**
    * Clear the stdout and stderr logs for the named process and reopen them
@@ -321,7 +321,7 @@ export interface SupervisordClientMethod {
    * @param {string} name The name of the process (or `group:name`)
    * @return {boolean} always returns True unless error
    */
-  clearProcessLogs: (name: string) => Promise<boolean>;
+  clearProcessLogs(name: string): Promise<boolean>;
 
   /**
    * Clear the stdout and stderr logs for the named process and reopen them
@@ -329,21 +329,21 @@ export interface SupervisordClientMethod {
    * @param {string} name The name of the process (or `group:name`)
    * @return {boolean} always returns True unless error
    */
-  clearProcessLog: (name: string) => Promise<boolean>;
+  clearProcessLog(name: string): Promise<boolean>;
 
   /**
    * Clear all process log files
    *
    * @return {ProcessStatusInfo[]} An array of process status info structs
    */
-  clearAllProcessLogs: () => Promise<ProcessStatusInfo[]>;
+  clearAllProcessLogs(): Promise<ProcessStatusInfo[]>;
 
   /**
    * Return an array listing the available method names
    *
    * @return {string[]} An array of method names available (strings)
    */
-  listMethods: () => Promise<string[]>;
+  listMethods(): Promise<string[]>;
 
   /**
    * Return a string showing the method's documentation
@@ -351,7 +351,7 @@ export interface SupervisordClientMethod {
    * @param {string} name The name of the method
    * @return {string} The documentation for the method name
    */
-  methodHelp: (name: string) => Promise<string>;
+  methodHelp(name: string): Promise<string>;
 
   /**
    * Return an array describing the method signature in the form [rtype, ptype, ptype…]
@@ -361,7 +361,7 @@ export interface SupervisordClientMethod {
    * @param {string} name The name of the method
    * @return {string[]} The result
    */
-  methodSignature: (name: string) => Promise<string[]>;
+  methodSignature(name: string): Promise<string[]>;
 
   /**
    * Process an array of calls, and return an array of results.
@@ -373,5 +373,5 @@ export interface SupervisordClientMethod {
    * @param {MulticallArgs[]} calls An array of call requests
    * @return {any} The result
    */
-  multicall: (calls: MulticallArgs[]) => Promise<any>;
+  multicall(calls: MulticallArgs[]): Promise<any>;
 }
